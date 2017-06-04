351ff53430ab54d9e7a64b94c0711cf22344859c
Grassi <grassiricardorg@gmail.com>
2017-06-03 23:15:18 -0300

    initial forms reactives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
+import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
-    TemplateFormModule
+    TemplateFormModule,
+    DataFormComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
