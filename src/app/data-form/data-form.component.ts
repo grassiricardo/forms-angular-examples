6b86b7bf0a18f81df636be8a432785864cecb12a
Grassi <grassiricardorg@gmail.com>
2017-07-05 00:26:30 -0300

    add setValue and patchValue and search cep in formbuilder

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -70,4 +70,53 @@ export class DataFormComponent implements OnInit {
       'has-feedback': this.verificaValidTouched(campo)
     };
   }
+
+  consultaCep() {
+
+    let cep = this.formulario.get('endereco.cep').value;
+
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.resetaDadosForm();
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .map(dados => dados.json())
+          .subscribe(dados => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+  populaDadosForm(dados) {
+    this.formulario.patchValue  ({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm() {
+    this.formulario.patchValue  ({
+      endereco: {
+        rua: null,
+        cep: null,
+        complemento:null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
+  }
 }
