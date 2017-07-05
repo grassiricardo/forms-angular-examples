9b9f6d70fcd4e0b4a57c039f8ebe81cec7dba0b8
Grassi <grassiricardorg@gmail.com>
2017-07-04 22:55:14 -0300

    add validation in inputs

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -42,4 +42,22 @@ export class DataFormComponent implements OnInit {
   resetar() {
     this.formulario.reset();
   }
+
+  verificaValidTouched(campo) {
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+
+  verificaEmailInvalido() {
+    let campoEmail = this.formulario.get('email');
+    if (campoEmail.errors) {
+      return campoEmail.errors['email'] && campoEmail.touched;
+    }
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    };
+  }
 }
